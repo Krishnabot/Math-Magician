971a3dce7c1f2c0b0c35ea5097344f06ef821c5a
Krishna <libraryismyparadise@gmail.com>
2022-08-21 07:36:32 +0545

    Add Home Page

---
+++ src/pages/Home.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="container">
+    <h2>Welcome to our page!</h2>
+    <p className="intro-1">
+      Mathematics is one of the common subjects that we study since our
+      childhood. It is generally used in our daily life. Every person needs to
+      learn some basics of it. Even counting money also includes math. Every
+      work is linked with math in some way or the other. A person who does math
+      is called a Mathematician.
+      <br />
+      Mathematics can be divided into two parts. The first is Pure mathematics,
+      and the second is Applied mathematics. In Pure mathematics, we need to
+      study the basic concept and structures of mathematics. But, on the other
+      side, Applied mathematics involves the application of mathematics to solve
+      problems that arise in various areas,(e.g.), science, engineering, and so
+      on.
+      <br />
+      One couldn’t imagine the world without math. Math makes our life
+      systematic, and every invention involves math. No matter what action a
+      person is doing, he should know some basic maths. Every profession
+      involves maths. Our present-day world runs on computers, and even computer
+      runs with the help of maths. Every development that happens requires math.
+    </p>
+    <p className="intro-2">
+      Math is complicated to learn, and one needs to focus and concentrate more.
+      Math is logical sometimes, and the logic needs to be derived out. Maths
+      make our life easier and more straightforward. Math is considered to be
+      challenging because it consists of many formulas that have to be learned,
+      and many symbols and each symbol generally has its significance.
+      <br />
+      Some ofthe advantages of Math in our daily life
+    </p>
+    <ul>
+      <li>
+        Managing Money: Counting money and calculating simple interest, compound
+        interest includes the usage of mathematics. Profit and loss are also
+        computed using maths. Anything related to maths contains maths.
+      </li>
+      <li>
+        Cooking: Maths is even used in cooking as estimating the number of
+        ingredients that have to be used is calculated in numbers. Proportions
+        also include maths.
+      </li>
+      <li>
+        Home modelling: Calculating the area is essential in the construction of
+        the home or home modelling. The size is also measured using maths. Even
+        heights are also measured using maths.
+      </li>
+      <li>
+        Travelling: Distance between two places and time taken to travel also
+        includes maths. The amount of time taken revolves around maths. Almost
+        every work is related to maths in some way or another. Maths contains
+        some conditions that need to be followed, and maths has several formulas
+        that have to be learned to become a mathematician.
+      </li>
+    </ul>
+  </div>
+);
+
+export default Home;
